cd123905627a1d1b8c0d56054a57358db4610b57
DavideSias <davidesias77@gmail.com>
2022-12-07 11:33:43 +0100

    Add section of footer

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,5 +1,11 @@
 <template>
-  <footer />
+  <footer>
+    <div class="work-footer">
+      <h2>LET'S TALK ABOUT WORK</h2>
+      <p>A wonderful serenity has taken possession far far away, behind the word mountains.</p>
+      <button>get started</button>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -9,5 +15,42 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/assets/scss/variables';
+.work-footer{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  height: 45vh;
+  background-image: url('@/img/business-people-working-together-on-project-and-B3MZ4TX.jpg');
+  background-position: center;
 
+  h2{
+    font-size: 3rem;
+    margin-bottom: 1rem;
+  }
+
+  p{
+    text-align: center;
+    max-width: 25%;
+    font-size: 1.2rem;
+    margin-bottom: 2rem;
+  }
+
+  button{
+      padding: 1.5rem 3rem;
+      background-color: $brand-color;
+      color: white;
+      letter-spacing: 0.2rem;
+      font-weight: 600;
+      text-transform: uppercase;
+      border: none;
+      cursor: pointer;
+
+      &:hover{
+        opacity: 0.8;
+      }
+}
+}
 </style>
